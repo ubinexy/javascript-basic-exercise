534a02617cde1dd2778a685fe4a9b2eca7c87248
ubinexy <ubinexy@gmail.com>
2020-07-27 01:29:56 +0800

    Exercise04: done

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,20 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if(array == undefined) {
+    throw('Creating list from undefined array');
+  }
+  if(array.length === 0) {
+    throw('Creating list from empty array');
+  }
+
+  const head = {next: null, value: array[0]};
+  let curr = head;
+
+  for(let i=1; i < array.length; i++) {
+    let node = {next: null, value: array[i]};
+    curr.next = node;
+    curr = node;
+  }
+  return head;
 }
