21b5a384c2552b3f081ec86e1540b7a1d6ac9a62
ubinexy <ubinexy@gmail.com>
2020-07-27 01:59:03 +0800

    Exercise05: done

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,11 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if(array == undefined)
+    throw('Flatten undefined or null array');
+  
+  if(array.length === 0) 
+    return [];
+
+  return array.reduce((flattenArr, curr)=> flattenArr.concat(curr), []);
 }
