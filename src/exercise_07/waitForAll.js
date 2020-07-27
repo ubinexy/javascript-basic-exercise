c37961da99cd10f07b82524719e1976727b2e9e3
ubinexy <ubinexy@gmail.com>
2020-07-27 23:55:12 +0800

    Exercise07: done

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -9,5 +9,27 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  for (let p of promises)  {
+    if(!(p instanceof Promise)) {
+      throw('Not all elements are promises.');
+    }
+  }
+
+  let status = true;
+
+  const all = promises.map(p => p
+  .then(
+    () => {},
+    () => {status = false;}
+  ));
+  
+  return Promise.all(all).then(
+    () => {
+      if(status) {
+        return Promise.resolve('');
+      } else {
+        return Promise.reject('');
+      }
+    }
+  ); 
 }
